f42fd46d83fcc8cd2d91cfbc0ee933c22cdd1d15
shijinhua <shijinhua@yatang.cn>
2017-10-13 17:06:33 +0800

    'add'

--- server/server.sh
+++ server/server.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then  
+    name='pro'  
+else if [ $1 -eq 'pro' -o $1 -eq 'dev' -o $1 -eq 'test' ]; then
+    name=$1
+else
+    name='pro'
+fi
+
 echo "delete dist"
 rm -rf /var/web/active-cms/server/dist
+
 echo "start..."
 cd /var/web/active-cms/server
+
 echo "pull..."
 git pull
+
 echo "install..."
-#npm install
+cnpm install
+
 echo "build..."
 npm run prestart:prod
+
 echo "stop pm2..."
 pm2 stop all
-#echo "move..."
-#rm -rf /var/web/dist && mv /var/web/active-cms/server/dist /var/web/dist
-echo "cd dist/"
-#cd /var/web/dist
+
 echo "start pm2 ..."
-npm run pm2-dev
-pm2 list
-echo "ok..."
+pm2 start ./pm2.config.js --env ${name}
+
+echo "启动成功"
