a1728e304d5c87e2591ecfe299fada0b7e3ff8ca
shijinhua <shijinhua@yatang.cn>
2017-10-13 15:10:27 +0800

    'add'

--- server/src/config/environment/development.js
+++ server/src/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost:27017/nodemongo-dev',
+    uri: 'mongodb://localhost/nodemongo-dev',
     options: {
         useMongoClient: true
     }
