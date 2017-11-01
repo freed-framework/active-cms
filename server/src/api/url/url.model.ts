6ae99ef582b29666cef327e42f43f0a72991b687
shijinhua <shijinhua@yatang.cn>
2017-11-01 11:04:49 +0800

    cuole

--- server/src/api/url/url.model.ts
+++ server/src/api/url/url.model.ts
@@ -9,24 +9,9 @@ import * as mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const UrlScgema = new Schema({
-    // 对应用户
-    ower: { type: Schema.Types.ObjectId, ref: 'Users' },
+const UrlSchema = new Schema({
+    // folder 连接
 
-    // 对应页面
-    page: { type: Schema.Types.ObjectId, ref: 'Pages', default: null },
-
-    // 文件夹名
-    name: { type: String },
-
-    // 创建时间
-    createTime: { type: Date, default: Date.now },
-
-    // 层级
-    level: { type: Number, default: 0 },
-
-    // 子文件夹
-    childrens: [{ type: Schema.Types.ObjectId, ref: 'Urls' }]
 })
 
-export default mongoose.model('Urls', UrlScgema);
+export default mongoose.model('Urls', UrlSchema);
