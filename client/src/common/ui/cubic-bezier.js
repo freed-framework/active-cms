138f87abbe2837beb88fea05cd7406945b39c3e3
denglingbo <denglingbo@126.com>
2017-12-04 14:01:47 +0800

    bezier

---
+++ client/src/common/ui/cubic-bezier.js
@@ -0,0 +1,39 @@
+/**
+ * @file cubic-bezier.js
+ * @author denglingbo
+ *
+ */
+
+const fn = {
+    'ease': '.25, .1, .25, 1',
+    'linear': '0, 0, 1, 1',
+    'ease-in': '.42,0,1,1',
+    'ease-out': '0,0,.58,1',
+    'ease-in-out': '.42,0,.58,1',
+    'ease-in-sine': '.47, 0, .745, .715',
+    'ease-out-sine': '.39, .575, .565, 1',
+    'ease-in-out-sine': '.445, .05, .55, .95',
+    'ease-in-quad': '.55, .085, .68, .53',
+    'ease-out-quad': '.25, .46, .45, .94',
+    'ease-in-out-quad': '.455, .03, .515, .955',
+    'ease-in-cubic': '.55, .055, .675, .19',
+    'ease-out-cubic': '.215, .61, .355, 1',
+    'ease-in-out-cubic': '.645, .045, .355, 1',
+    'ease-in-quart': '.895, .03, .685, .22',
+    'ease-out-quart': '.165, .84, .44, 1',
+    'ease-in-out-quart': '.77, 0, .175, 1',
+    'ease-in-quint': '.755, .05, .855, .06',
+    'ease-out-quint': '.23, 1, .32, 1',
+    'ease-in-out-quint': '.86, 0, .07, 1',
+    'ease-in-expo': '.95, .05, .795, .035',
+    'ease-out-expo': '.19, 1, .22, 1',
+    'ease-in-out-expo': '1, 0, 0, 1',
+    'ease-in-circ': '.6, .04, .98, .335',
+    'ease-out-circ': '.075, .82, .165, 1',
+    'ease-in-out-circ': '.785, .135, .15, .86',
+    'ease-in-back': '.6, -.28, .735, .045',
+    'ease-out-back': '.175, .885, .32, 1.275',
+    'ease-in-out-back': '.68, -.55, .265, 1.55'
+}
+
+
