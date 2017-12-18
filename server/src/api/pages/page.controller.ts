fd99d1011750fb422cc531dc36f7998ea3e26e84
shijinhua <shijinhua@yatang.cn>
2017-12-18 09:05:14 +0800

    修改登录注册

--- server/src/api/pages/page.controller.ts
+++ server/src/api/pages/page.controller.ts
@@ -26,14 +26,19 @@ export class PageController {
     @Get('/remove/:id')
     async removePage(@Request() req, @Response() res, @Param('id') id) {
         const page: any = await this.service.getPage(id);
-        const { user } = req;
+        const { user, logger } = req;
 
         if (!page || `${user._id}` != `${page.owerUser}`) {
             throw new Exception('删除页面失败', 500);
+
+            logger.error("%s 删除页面失败, 页面id： %s, 时间： %s", user.userDspName, id, new Date())
         }
         else {
             let result = await this.service.removePage(id);
             result = CommonService.commonResponse(result);
+
+            logger.info("%s 删除页面成功, 页面id： %s, 时间： %s", user.userDspName, id, new Date())
+
             res.status(HttpStatus.OK).json(result);
         }
     }
@@ -51,6 +56,7 @@ export class PageController {
         const { query, user } = req;
         const { content = '' } = query;
         let result = await this.service.pagingQuery(query, {owerUser: user._id, title: {$regex: content, $options: 'i'}})
+
         res.status(HttpStatus.OK).json(CommonService.commonResponse(result));
     }
 
@@ -88,55 +94,81 @@ export class PageController {
 
     @Post('/update/fork')
     async updateFork(@Request() req, @Response() res, @Body() body) {
+        const { user, logger } = req;
         const { id, title } = body;
-        const { user } = req;
         const result = await this.service.updateFork(user._id, id, title);
+
+        logger.info("%s 复制页面成功, 页面id： %s, 时间： %s", user.userDspName, id, new Date())
+
         res.status(HttpStatus.OK).json(result);
     }
 
     @Post('/update')
     @UsePipes(new encodeUpdatePipe())
-    async updatePage(@Response() res, @Body() body) {
+    async updatePage(@Request() req,@Response() res, @Body() body) {
+        const { user, logger } = req;
         const { id, page } = body;
-        const result = await this.service.updatePage(id, page);
+        const result: any = await this.service.updatePage(id, page);
+
+        logger.info("%s 更新页面成功, 页面id： %s, 时间： %s", user.userDspName, id, new Date())
+
         res.status(HttpStatus.OK).json(result);
     }
 
     @Post()
     @UsePipes(new encodeCreatePipe())
     async addPage(@Request() req, @Response() res, @Body() body: CreatePageDto) {
-        const { user } = req;
-        const result = await this.service.addPage({
+        const { user, logger } = req;
+        const result: any = await this.service.addPage({
             body,
             createUser: user._id,
             owerUser: user._id
         });
+
+        logger.info("%s 新建页面成功, 页面id： %s, 时间： %s", user.userDspName, result._id, new Date())
+
         res.status(HttpStatus.OK).json(result);
     }
 
     @Post('/changeTitle')
-    async changeTitle(@Response() res, @Body() body) {
+    async changeTitle(@Request() req, @Response() res, @Body() body) {
+        const { user, logger } = req;
         const { id, title } = body;
-        const result = await this.service.changeTitle(id, title);
+        const result: any = await this.service.changeTitle(id, title);
+
+        logger.info("%s 修改标题成功, 页面id： %s, 时间： %s", user.userDspName, result._id, new Date())
+
         res.status(HttpStatus.OK).json(result);
     }
 
     @Get('/push/:id/:pushId')
-    async pushId(@Response() res, @Body() body, @Param() param) {
+    async pushId(@Request() req, @Response() res, @Body() body, @Param() param) {
+        const { user, logger } = req;
         const { id, pushId } = param;
-        const result = await this.service.pushId(id, pushId);
+        const result: any = await this.service.pushId(id, pushId);
+
+        logger.info("%s 发布页面成功, 页面id： %s, 时间： %s", user.userDspName, result._id, new Date())
+
         res.status(HttpStatus.OK).json(result);
     }
 
     @Post('/push')
-    async push(@Response() res, @Body() body) {
-        const result: any = await this.service.push(body);
+    async push(@Request() req, @Response() res, @Body() body) {
+        const { user, logger } = req;
+        const result: any = await this.service.push(body, user._id);
+
+        logger.info("%s 发布页面成功, 页面id： %s, 时间： %s", user.userDspName, result._id, new Date())
+
         res.status(HttpStatus.OK).json(result);
     }
 
     @Get('/delete/push/:id')
-    async pushDelete(@Response() res, @Body() body, @Param('id') id) {
+    async pushDelete(@Request() req, @Response() res, @Body() body, @Param('id') id) {
+        const { user, logger } = req;
         const result: any = await this.service.pushDelete(id);
+
+        logger.info("%s 删除服务器静态资源成功, 页面id： %s, 时间： %s", user.userDspName, result._id, new Date())
+
         res.status(HttpStatus.OK).json(result);
     }
 
