16644315da1c5e0bbe49970006af05ce67fadb04
shijinhua <shijinhua@yatang.cn>
2018-01-09 16:29:30 +0800

    merge

--- client/config/config.js
+++ client/config/config.js
@@ -3,7 +3,7 @@
  * @Author: shijh
  * @CreateDate: 2017-12-25 09:48:26
  * @Last Modified by: shijh
- * @Last Modified time: 2018-01-09 15:55:38
+ * @Last Modified time: 2018-01-09 15:56:29
  *
  * 正式 环境全局变量
  */
