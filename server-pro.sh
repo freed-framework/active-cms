76abdbb1221772d9b83ec74e7793741873a79f0f
shijinhua <shijinhua@yatang.cn>
2018-01-18 17:36:50 +0800

    修改地址源

--- server-pro.sh
+++ server-pro.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "start..."
+cd /var/web/active-cms
+
+echo "pull..."
+git pull
+
+
 # pro启动
 sh ./server.sh pro
--- server-test.sh
+++ server-test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "start..."
+cd /var/web/active-cms
+
+echo "pull..."
+git pull origin dev-client
+
+
 # test启动
 sh ./server.sh test
