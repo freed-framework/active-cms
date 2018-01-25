a3b7f223f76c508fb90b395c8db43f9e2fbb9dcd
shijinhua <shijinhua@yatang.cn>
2018-01-25 15:35:47 +0800

    merge

--- client/config/config.test.js
+++ client/config/config.test.js
@@ -3,14 +3,14 @@
  * @Author: shijh
  * @CreateDate: 2017-12-25 09:48:26
  * @Last Modified by: shijh
- * @Last Modified time: 2018-01-08 11:53:12
+ * @Last Modified time: 2018-01-25 15:35:34
  *
  * test 环境全局变量
  */
 
 window.config = {
     api: 'http://sitxcsc.yatang.com.cn/api/sc',
-    domain: 'http://www.iting.top',
+    domain: 'http://wuget-uat.yatang.com.cn',
     // domain: 'http://localhost:3000',
     // 上传页面预览地址
     html: 'http://sit.db.com/html/'
