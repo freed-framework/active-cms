a99940ef2e3b5952a9853698eaa73feae0dd2fb8
shijinhua <shijinhua@yatang.cn>
2018-01-30 13:57:04 +0800

    merge

--- client/components/mobile/click-area/ClickArea.d.ts
+++ client/components/mobile/click-area/ClickArea.d.ts
@@ -3,6 +3,12 @@
 interface ClickAreaProps extends DefaultProps {
     url?: string;
     componentProps?: any;
+    modalWidth?: string | number;
+    modalTop?: string | number;
+    modalBtn?: string | number;
+    modalContent: string | JSX.Element;
+    showExplain?: boolean;
+    hasModal?: boolean;
 }
 
 declare function escape(message: string): any;
